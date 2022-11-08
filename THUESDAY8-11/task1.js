d5bd4b9fbe701f61cfa7d67ad7b1ae2fd186e09b
unknown <wal.obeidat@gmail.com>
2022-11-08 12:54:04 -0800

    task

---
+++ THUESDAY8-11/task1.js
@@ -0,0 +1,114 @@
+"use strict";
+///1///
+
+// [ 1,7 , 9 , 45 ]
+
+//  ["Str" ,"alex","moh",
+
+//  'the','fox', 'over',' lazy', 'dog']
+
+///2///
+
+// Whattheindexof = "Banana","Tomato"
+// var fruits=["Tomato","Banana","Watermelon"]
+// tomato:0     banana:1
+
+///3///
+
+// 1- Favorite Food (5)
+// 2- Favorite Sport (3)
+// 3- Favorite Movie (4)
+// Favorite=['Vegetables','Eggs','Dairy foods','Grains','legumes']
+// Favorite=['Basketball','Boxing','Curling']
+// Favorite=['Action ','Drama ','Comedy','Fantasy']
+
+///4///
+
+// function firstOfArray(arr)
+// {
+//     return arr[0]
+
+//  }
+//     console.log(firstOfArray([1,2,3]))
+//     console.log(firstOfArray (['t','u', 'g','x']));
+
+     ///5///
+    // function lastOfArray(arr)
+    // {
+    //     return arr[arr.length -1]
+    
+    //  }
+    //     console.log(lastOfArray([1,4,5]))
+    //     console.log(lastOfArray (['t','u', 'g','x']));
+
+    ///6///
+
+    // Using console make this array to be like this one (push, unshift, shift, pop)
+//     var array = [0,5,7,9];
+// array.shift([0]);
+// array.shift([1]);
+// array.shift([2]);
+// array.push(10);
+// array.unshift(1,3,4,6,8);
+// console.log(array);
+
+ ///7///
+
+// var array2 = [5 , 9 ,-7 , 3.5 ];
+
+// console.log(array2.push(200));
+// console.log(array2);
+// console.log(array2.unshift(100));
+// console.log(array2);
+// console.log(array2.pop());
+// console.log(array2);
+// console.log(array2.shift());
+// console.log(array2);
+
+////8////
+
+// function middleOfArray (array) {
+//   if ( array.length % 2 == 0){
+//     return array.splice(array.length/2 - 1 , array.length/2 )
+//   }
+//   else {
+//     return array[Math.floor(array.length/2)]
+//   }
+// }
+
+// console.log(middleOfArray(["t","u","x"]));
+
+////9////
+
+ // function middleOfArray (array) {
+//   if ( array.length % 2 == 0){
+//     return array.splice(array.length/2 - 1 , array.length/2 )
+//   }
+//   else {
+//     return array[Math.floor(array.length/2)]
+//   }
+// }
+
+// console.log(middleOfArray(["t","u","x"]));
+ 
+////10////
+// var nums= [1,2,3,8,9]
+// function indexOfArray(nums, pos){
+//     return nums[pos]
+// }
+// console.log(indexOfArray(nums,3))
+// console.log(indexOfArray(nums,1))
+// console.log(indexOfArray(nums,4))
+
+    
+       
+      
+       
+
+   
+    
+
+
+
+  
+
---
+++ monday7-11/main1.js
@@ -0,0 +1,136 @@
+"use strict";
+////1////
+
+// function tellFortune (job_title,geographic_location,partners_name,number_of_children) {
+//     // "You will be a ____ in ___, and married to ____ with ____ kids."
+//                 //   شو لازم اعمل مشان يكتب لالي النص
+              
+                
+//              let name = 'You will be a ' + job_title + ' in ' + geographic_location + ', and married to ' +partners_name
+//                     + ' ' + ' with ' + number_of_children + ' kids.';
+//                    return name;
+// }
+//                 console.log(tellFortune ('Accounting','Amman','atef','5'));
+
+////2////
+
+// function calculateDogAge (age)
+//  {
+//     let result = age  * 7 ;
+//     console.log( ' your dog age is ' +result);
+// }
+// calculateDogAge(2)
+// calculateDogAge(4)
+
+////3////
+// function calculateSupply(age, numPerDay) {
+//     var maxAge = 100;
+//     var totalNeeded = (numPerDay * 365) * (maxAge - age);
+//     var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+//     console.log(message);
+//   }
+  
+//   calculateSupply(30, 3);
+//   calculateSupply(28, 2.5);
+//   calculateSupply(28, 400);
+
+   ///4///
+
+//    function greet(name) {
+//     console.log("Hello, " + name );
+//   }
+  
+//   greet("walaa");
+
+////5////
+//  CalculationEngineengine = new CalculationEngine ();
+//  {
+// Func<double, double, double> formula =(Func<double, double, double>)  engine.Function("x1+2/(3*x2)")
+// Parameter("x1", DataType.FloatingPoint);
+// Parameter("x2", DataType.FloatingPoint);
+// Result(DataType.FloatingPoint);
+// Build();
+// }
+
+////6////
+// lastOfArray ( [ "Breaking bad" , "prision break" , "fast and ferious"]);
+// function lastOfArray(x) {
+//  console.log (x[-1]);
+// }
+
+////7////
+// function cube(x)
+// {
+//     console.log(Math.pow(x, 3))
+// }
+// cube(4);
+
+//   ////8////
+    
+// function multiply(x, y){
+//    return x * y
+// }
+// console.log(multiply(3,4))
+
+ ////9////
+//  function canIGetADrivingLicense(age){
+//    if(age >= 20){
+//        console.log('yes you can')
+//    }else{
+//        rest = 20 - age;
+//        result= `please come back after ${rest} years to get one`
+//        console.log(result)
+//    }
+// }
+// canIGetADrivingLicense(30)
+
+// ////10////
+
+// function sameLength(str1, str2){
+//     let result = str1.length - str2.length
+//     console.log(result)
+//     if(!result){
+//         console.log('true')
+//     }else{
+//         console.log('false')
+//     }
+// }
+
+// sameLength("tree","clue")
+// sameLength("Huda","Mohammad")
+// sameLength("tree","car")
+
+// ///////////21/////////
+
+// function randomBetweenNumbers(num1, num2) {
+    
+//     x = Math.random() * (num1 - num2) +num2;
+    
+//     x = Math.floor(x)
+    
+//     console.log(x)
+// }
+
+// ////////13///////
+//  let x ="walaa obeidat";
+//  let y = "school" ;
+//  let z = "github";
+// let n = "noor";
+// let e = "israa";
+//  shortstring(x,y,z,n,e);
+
+// function shortstring(x,y,z,n,e);
+// {
+// if (x.length<y.length&&x.length<z.length&&x.length<n.length&&x.length<e.length) 
+//  {
+//   console.log(x)  
+// }
+// if (y.length<x.length&&y.length<z.length&&y.length<n.length&&y.length<e.length) 
+//  console.log(y)
+//  if (z.length<x.length&&z.length<y.length&&z.length<n.length&&z.length<e.length) 
+//  console.log(z)
+//  if (n.length<x.length&&n.length<y.length&&n.length<z.length&&.nlength<e.length) 
+//  console.log(n)
+//  if (e.length<x.length&&e.length<y.length&&e.length<z.length&&e.length<n.length) 
+//  console.log(n)
+// }
